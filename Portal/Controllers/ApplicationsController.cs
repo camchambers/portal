fdf89d5ba61a2354e689657fd189829e535d9d74
Cam Chambers <cam_chambers@outlook.com>
2021-07-27 10:45:37 -0400

    Added an application controller

---
+++ Portal/Controllers/ApplicationsController.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using portal.Data;
+using portal.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace portal.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Applications
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Application.OrderBy(m => m.Weight).ToListAsync());
+        }
+
+        // GET: Applications/Admin
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> Admin()
+        {
+            return View(await _context.Application.ToListAsync());
+        }
+
+        // GET: Applications/Details/5
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Application
+                .FirstOrDefaultAsync(m => m.Aid == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // GET: Applications/Create
+        [Authorize]
+        [Authorize(Roles = "SuperAdmin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Applications/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> Create([Bind("Aid,Name,Image,Link,Weight,Disabled")] Application application)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(application);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(application);
+        }
+
+        // GET: Applications/Edit/5
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Application.FindAsync(id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+            return View(application);
+        }
+
+        // POST: Applications/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> Edit(int id, [Bind("Aid,Name,Image,Link,Weight,Disabled")] Application application)
+        {
+            if (id != application.Aid)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(application);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApplicationExists(application.Aid))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(application);
+        }
+
+        // GET: Applications/Delete/5
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var application = await _context.Application
+                .FirstOrDefaultAsync(m => m.Aid == id);
+            if (application == null)
+            {
+                return NotFound();
+            }
+
+            return View(application);
+        }
+
+        // POST: Applications/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "SuperAdmin")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var application = await _context.Application.FindAsync(id);
+            _context.Application.Remove(application);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApplicationExists(int id)
+        {
+            return _context.Application.Any(e => e.Aid == id);
+        }
+    }
+}
