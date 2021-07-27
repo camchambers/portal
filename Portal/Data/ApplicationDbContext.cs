2740dc27077cf1d5433baa498a51258974913307
Cam Chambers <cam_chambers@outlook.com>
2021-07-27 10:46:54 -0400

    Updated DbContext

--- Portal/Data/ApplicationDbContext.cs
+++ Portal/Data/ApplicationDbContext.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using portal.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace portal.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<Application> Application { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
