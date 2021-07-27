3467796854ccd4c8451ec42335e0cb1c181abff6
Cam Chambers <cam_chambers@outlook.com>
2021-07-27 10:45:19 -0400

    Created an application model

---
+++ Portal/Models/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace portal.Models
+{
+    public partial class Application
+    {
+        [Key]
+        public int Aid { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Link { get; set; }
+        public int? Weight { get; set; }
+        public bool? Disabled { get; set; }
+    }
+}
