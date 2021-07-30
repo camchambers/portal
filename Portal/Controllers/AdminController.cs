d6f2121ce877d0a4a888507dd85a8cb1ff7935fb
Cam Chambers <cam_chambers@outlook.com>
2021-07-30 08:16:22 -0400

    Created an admin controller for admin tasks

---
+++ Portal/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using adv_portal.Data;
+using adv_portal.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace adv_portal.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Applications
+        [Authorize(Roles = "SuperAdmin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
