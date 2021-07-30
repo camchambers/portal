e9483aca4c21a47e4b4816a5a69e447f042b9495
Cam Chambers <cam_chambers@outlook.com>
2021-07-30 08:36:45 -0400

    Created an Asset and Asset Type controller

---
+++ Portal/Models/Asset.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace portal.Models
+{
+    public class Asset
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Type")]
+        public string AssetType { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Serial { get; set; }
+        [DisplayName("Memory (GB)")]
+        public int? Memory { get; set; }
+        [DisplayName("Storage (GB)")]
+        public int? Storage { get; set; }
+        public string Comment { get; set; }
+        [DisplayName("Owner")]
+        public string UserID { get; set; }
+
+        public Asset()
+        {
+        }
+    }
+}
